a5dd11423cf20070895728bb468e110fc73a2ef3
linnefromice <haruyama.arata@moneyforward.co.jp>
2021-11-27 23:46:01 +0900

    training call external contract

---
+++ block-academy/12/Bank.sol
@@ -0,0 +1,33 @@
+pragma solidity 0.8.10;
+
+contract Bank {
+
+  event balanceUpdate(string _txType, address _owner, uint _amount);
+
+  mapping(address => uint) balance;
+
+  function getBalance() external view returns(uint) {
+    return balance[msg.sender];
+  }
+
+  function deposit() external payable {
+    balance[msg.sender] += msg.value;
+    emit balanceUpdate("Deposit", msg.sender, balance[msg.sender]);
+  }
+
+  function withdraw(uint _amount) external {
+    require(balance[msg.sender] >= _amount, "Insufficient balance");
+    balance[msg.sender] -= _amount;
+    payable(msg.sender).transfer(_amount);
+    emit balanceUpdate("Withdraw", msg.sender, balance[msg.sender]);
+  }
+
+  function transferFrom(address _from, address _to, uint _amount) external {
+    require(balance[_from] >= _amount, "Insufficient balance");
+    require(_from != _to, "Invalid recipient");
+    balance[_from] -= _amount;
+    balance[_to] += _amount;
+    emit balanceUpdate("Outgoing Transfer", _from, balance[_from]);
+    emit balanceUpdate("Incoming Transfer", _to, balance[_to]);
+  }
+}
