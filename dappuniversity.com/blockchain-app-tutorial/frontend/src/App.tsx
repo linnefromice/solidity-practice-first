e1a4ce3b4436683cd80e6752cb02dde2e5713819
linnefromice <linnefromice@gmail.com>
2021-12-09 00:54:43 +0900

    update component to use contract's function

--- dappuniversity.com/blockchain-app-tutorial/frontend/src/App.tsx
+++ dappuniversity.com/blockchain-app-tutorial/frontend/src/App.tsx
@@ -1,18 +1,89 @@
 import React, { useEffect, useState, VFC } from "react";
 import { ethers } from "ethers";
 
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // update every time We deploy contract / copy from log (`npx hardhat run (file for deployment)`)
+const abi = [ // update every time We deploy contract / copy from typechain/factories/TodoList__factory.ts
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_content",
+        type: "string",
+      },
+    ],
+    name: "createTask",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "taskCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "tasks",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "content",
+        type: "string",
+      },
+      {
+        internalType: "bool",
+        name: "completed",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
 export const App: VFC = () => {
   const provider = new ethers.providers.JsonRpcProvider();
-  const signer = provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, abi, provider);
+  const { taskCount } = contract.functions;
 
   const [addresses, setAddresses] = useState<string[]>([]);
+  const [taskCountValue, setTaskCountValue] = useState<string>("");
 
   useEffect(() => {
     const getAddresses = async () => {
       const addresses = await provider.listAccounts();
       setAddresses(addresses);
     }
+    const getTaskCount = async () => {
+      const _taskCount = await taskCount()
+      setTaskCountValue(_taskCount);
+    }
     getAddresses();
+    getTaskCount();
   }, [])
 
   return (
@@ -21,6 +92,7 @@ export const App: VFC = () => {
       <ul>
         {addresses.map((addr, index) => <ol key={index}>{addr}</ol>)}
       </ul>
+      <p>{`taskCount ... ${taskCountValue}`}</p>
     </div>
   );
 }
