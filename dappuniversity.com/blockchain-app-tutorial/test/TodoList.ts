00576b48272fe013c19672684f1a1b3a6cf4a784
linnefromice <linnefromice@gmail.com>
2021-12-08 10:29:16 +0900

    add test

---
+++ dappuniversity.com/blockchain-app-tutorial/test/TodoList.ts
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("TodoList", () => {
+  it("Should has one task in task list provided by constructor", async () => {
+    const TodoList = await ethers.getContractFactory("TodoList");
+    const todoList = await TodoList.deploy();
+    await todoList.deployed();
+
+    expect(await todoList.taskCount()).to.equal(1);
+  })
+});
