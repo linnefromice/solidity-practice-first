d7d1ef5038ce6d7d180a6f69ae7736c7b35ec9dc
linnefromice <linnefromice@gmail.com>
2021-12-09 21:45:37 +0900

    refactor contract's test - add case to complete task

--- dappuniversity.com/blockchain-app-tutorial/test/TodoList.ts
+++ dappuniversity.com/blockchain-app-tutorial/test/TodoList.ts
@@ -20,13 +20,25 @@ describe("TodoList", () => {
       .withArgs(4, "Test Task", false);
   });
 
-  it("toggles task completion", async () => {
-    const TodoList = await ethers.getContractFactory("TodoList");
-    const todoList = await TodoList.deploy();
-    await todoList.deployed();
+  describe("toggles task completion", async () => {
+    it("emit event named `TaskCompleted`", async () => {
+      const TodoList = await ethers.getContractFactory("TodoList");
+      const todoList = await TodoList.deploy();
+      await todoList.deployed();
+        await expect(todoList.toggleCompleted(1))
+        .to.emit(todoList, "TaskCompleted")
+        .withArgs(1, true);
+    });
+    it("complete task", async () => {
+      const TodoList = await ethers.getContractFactory("TodoList");
+      const todoList = await TodoList.deploy();
+      await todoList.deployed();
 
-    await expect(todoList.toggleCompleted(1))
-      .to.emit(todoList, "TaskCompleted")
-      .withArgs(1, true);
+      await todoList.toggleCompleted(1);
+      expect((await todoList.tasks(1)).completed).to.equal(true);
+
+      await todoList.toggleCompleted(1);
+      expect((await todoList.tasks(1)).completed).to.equal(false);
+    })
   })
 });
