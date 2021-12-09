3d0c058dbf0c961feb521197d15df99bddce2a44
linnefromice <linnefromice@gmail.com>
2021-12-09 10:22:04 +0900

    update test - confirm emitted event

--- dappuniversity.com/blockchain-app-tutorial/test/TodoList.ts
+++ dappuniversity.com/blockchain-app-tutorial/test/TodoList.ts
@@ -8,5 +8,15 @@ describe("TodoList", () => {
     await todoList.deployed();
 
     expect(await todoList.taskCount()).to.equal(1);
-  })
+  });
+
+  it("Should emit event named TaskCreated by function createTask", async () => {
+    const TodoList = await ethers.getContractFactory("TodoList");
+    const todoList = await TodoList.deploy();
+    await todoList.deployed();
+
+    await expect(todoList.createTask("Test Task"))
+      .to.emit(todoList, "TaskCreated")
+      .withArgs(2, "Test Task", false);
+  });
 });
