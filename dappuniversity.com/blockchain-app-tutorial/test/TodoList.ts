158df2af046989f662683b24609a53e20ff137b7
linnefromice <linnefromice@gmail.com>
2021-12-09 21:39:42 +0900

    add test for completion to task

--- dappuniversity.com/blockchain-app-tutorial/test/TodoList.ts
+++ dappuniversity.com/blockchain-app-tutorial/test/TodoList.ts
@@ -2,12 +2,12 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("TodoList", () => {
-  it("Should has one task in task list provided by constructor", async () => {
+  it("Should has three tasks in task list provided by constructor", async () => {
     const TodoList = await ethers.getContractFactory("TodoList");
     const todoList = await TodoList.deploy();
     await todoList.deployed();
 
-    expect(await todoList.taskCount()).to.equal(1);
+    expect(await todoList.taskCount()).to.equal(3);
   });
 
   it("Should emit event named TaskCreated by function createTask", async () => {
@@ -19,4 +19,14 @@ describe("TodoList", () => {
       .to.emit(todoList, "TaskCreated")
       .withArgs(2, "Test Task", false);
   });
+
+  it("toggles task completion", async () => {
+    const TodoList = await ethers.getContractFactory("TodoList");
+    const todoList = await TodoList.deploy();
+    await todoList.deployed();
+
+    await expect(todoList.toggleCompleted(1))
+      .to.emit(todoList, "TaskCompleted")
+      .withArgs(1, true);
+  })
 });
