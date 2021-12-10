defb1200e5f74d46cf80212a4b3f317ff0fa514d
linnefromice <linnefromice@gmail.com>
2021-12-10 09:19:11 +0900

    refactor test - modify createTask's test

--- dappuniversity.com/blockchain-app-tutorial/test/TodoList.ts
+++ dappuniversity.com/blockchain-app-tutorial/test/TodoList.ts
@@ -10,15 +10,27 @@ describe("TodoList", () => {
     expect(await todoList.taskCount()).to.equal(3);
   });
 
-  it("Should emit event named TaskCreated by function createTask", async () => {
-    const TodoList = await ethers.getContractFactory("TodoList");
-    const todoList = await TodoList.deploy();
-    await todoList.deployed();
+  describe("function createTask", async () => {
+    it("emit event named `TaskCreated`", async () => {
+      const TodoList = await ethers.getContractFactory("TodoList");
+      const todoList = await TodoList.deploy();
+      await todoList.deployed();
 
-    await expect(todoList.createTask("Test Task"))
+      await expect(todoList.createTask("Test Task"))
       .to.emit(todoList, "TaskCreated")
       .withArgs(4, "Test Task", false);
-  });
+    });
+    it("create task", async () => {
+      const TodoList = await ethers.getContractFactory("TodoList");
+      const todoList = await TodoList.deploy();
+      await todoList.deployed();
+
+      await todoList.createTask("first task");
+      expect((await todoList.tasks(4)).content).to.equal("first task");
+      await todoList.createTask("second task");
+      expect((await todoList.tasks(5)).content).to.equal("second task");
+    })
+  })
 
   describe("toggles task completion", async () => {
     it("emit event named `TaskCompleted`", async () => {
