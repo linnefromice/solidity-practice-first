d64dd3d8b2782fba22402b0af1b0ca2f3dd88d6b
linnefromice <linnefromice@gmail.com>
2022-01-22 21:04:29 +0900

    progressed 'Structs'

---
+++ solidity-by-example.org/structs/Todos.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "./StructDeclaration.sol";
+
+contract Todos {
+  Todo[] public todos;
+
+  function create(string memory _text) public {
+    // 3 ways to initialize a struct
+
+    // - calling it like a function
+    todos.push(Todo(_text, false));
+    // - key value mapping
+    todos.push(Todo({text: _text, completed: false}));
+    // - initialize an empty struct and then update it
+    Todo memory todo;
+    todo.text = _text;
+    todos.push(todo);
+  }
+
+  // Solidity automatically created a getter for 'todos' so
+  // you don't actually need this function.
+  function get(uint _index) public view returns (string memory text, bool completed) {
+    Todo storage todo = todos[_index];
+    return (todo.text, todo.completed);
+  }
+
+  // update text
+  function update(uint _index, string memory _text) public {
+    Todo storage todo = todos[_index];
+    todo.text = _text;
+  }
+
+  // update completed
+  function toggleCompleted(uint _index) public {
+    Todo storage todo = todos[_index];
+    todo.completed = !todo.completed;
+  }
+}
