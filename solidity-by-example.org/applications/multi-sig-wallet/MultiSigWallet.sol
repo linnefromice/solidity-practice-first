12577586683e2dfa05a2ac6d823c021e4a160470
linnefromice <linnefromice@gmail.com>
2022-01-25 20:38:15 +0900

    progressed 'Multi-Sig Wallet'

--- solidity-by-example.org/applications/multi-sig-wallet/MultiSigWallet.sol
+++ solidity-by-example.org/applications/multi-sig-wallet/MultiSigWallet.sol
@@ -72,5 +72,89 @@ contract MultiSigWallet {
     numConfirmationsRequired = _numConfirmationsRequired;
   }
 
-  // receive から
+  receive() external payable {
+    emit Deposit(msg.sender, msg.value, address(this).balance);
+  }
+
+  function submitTransaction(
+    address _to,
+    uint _value,
+    bytes memory _data
+  ) public onlyOwner {
+    uint txIndex = transactions.length;
+
+    transactions.push(
+      Transaction({
+        to: _to,
+        value: _value,
+        data: _data,
+        executed: false,
+        numConfirmations: 0
+      })
+    );
+    
+    emit SubmitTransaction(msg.sender, txIndex, _to, _value, _data);
+  }
+
+  function confirmTransaction(uint _txIndex) public onlyOwner txExists(_txIndex) notExecuted(_txIndex) notConfirmed(_txIndex) {
+    Transaction storage transaction = transactions[_txIndex];
+    transaction.numConfirmations += 1;
+    isConfirmed[_txIndex][msg.sender] = true;
+
+    emit ConfirmTransaction(msg.sender, _txIndex);
+  }
+
+  function executeTransaction(uint _txIndex) public onlyOwner txExists(_txIndex) notExecuted(_txIndex) {
+    Transaction storage transaction = transactions[_txIndex];
+
+    require(
+      transaction.numConfirmations >= numConfirmationsRequired,
+      "cannot execute tx"
+    );
+
+    transaction.executed = true;
+    (bool success, ) = transaction.to.call{value: transaction.value}(
+      transaction.data
+    );
+    require(success, "tx failed");
+    
+    emit ExecuteTransaction(msg.sender, _txIndex);
+  }
+
+  function revokeConfirmation(uint _txIndex) public onlyOwner txExists(_txIndex) notExecuted(_txIndex) {
+    Transaction storage transaction = transactions[_txIndex];
+
+    require(isConfirmed[_txIndex][msg.sender], "tx not confirmed");
+
+    transaction.numConfirmations -= 1;
+    isConfirmed[_txIndex][msg.sender] = false;
+
+    emit RevokeConfirmation(msg.sender, _txIndex);
+  }
+
+  function getOwners() public view returns (address[] memory) {
+    return owners;
+  }
+
+  function getTransactionCount() public view returns (uint) {
+    return transactions.length;
+  }
+
+  function getTransaction(uint _txIndex) public view returns (
+    address to,
+    uint value,
+    bytes memory data,
+    bool executed,
+    uint numConfirmations
+  ) {
+    Transaction storage transaction = transactions[_txIndex];
+
+    return (
+      transaction.to,
+      transaction.value,
+      transaction.data,
+      transaction.executed,
+      transaction.numConfirmations
+    );
+  }
 }
